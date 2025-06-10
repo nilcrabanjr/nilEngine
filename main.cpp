0532186ef097628dcc50c063851e12f4639b4b05
nilcrabanjr <rileytturner@gmail.com>
2025-06-10 18:16:27 +1000

    Cleaned main.cpp

--- main.cpp
+++ main.cpp
@@ -5,15 +5,5 @@
 using namespace std;
 
 int main() {
-	int i;
-	Quest a("Walk Around", "Testing movement capabilites.", NotStarted, Generic);
-	TalkQuest b("Talkin", "Having a yap", NotStarted, Generic);
-	Player Riley(Alive, "Riley", "Human", 10, 10, 2);
 
-	Riley.checkStatus();
-	Riley.kill();
-	cout << "\n";
-	Riley.checkStatus();
-	cout << "\n\n";
-	Riley.printPlayer();
 }
